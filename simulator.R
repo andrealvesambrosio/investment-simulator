cb897a6b1913755bba1942a23e9603447e7aef79
andre.ambrosio <andrealvesambrosio@gmail.com>
2020-10-12 21:53:58 -0300

    refactor: create an function to check inputs

--- simulator.R
+++ simulator.R
@@ -37,6 +37,61 @@ get_formula <- function(params){
               main_input = main_input))
 }
 
+check_inputs <- function(params){
+  qtd_input <- params %>%
+    purrr::map(~length(.)) %>%
+    unlist() %>%
+    sum()
+  
+  # Other conditions: all > 0
+  
+  # Check if has at least 3 inputs
+  if(qtd_input < 3){
+    value <- "Por favor, preencha pelo menos três campos."
+    status = "erro"
+    
+    # Check if has at least one input missing to simulate
+  } else if(qtd_input == 5){
+      value <- "Por favor, deixe uma ou duas variáveis em branco. Senão, não há o que calcular"
+      status = "erro"
+    
+    # Check if the relation isn't Anos vs Rentabilidade
+  } else if(is.null(params[['var']]) & is.null(params[['years']])){
+      value <- "Por favor, não deixe Anos e Rentabilidade em branco ao mesmo tempo."
+      status = "erro"
+    
+    # Check if the single simulate isn't Anos
+  } else if(qtd_input == 4 & is.null(params[['years']])){
+      value <- "Por favor, não deixe apenas o campo Anos em branco."
+      status = "erro"
+    
+    # Check if the single simulate isn't Rentabilidade
+  } else if(qtd_input == 4 & is.null(params[['var']])){
+      value <- "Por favor, não deixe apenas o campo Rentabilidade em branco."
+      status = "erro"
+    
+    # Check if Total Money < Start
+  } else if((!is.null(params[['total_money']]) & 
+             !is.null(params[['start']]))){
+      if(params[['total_money']] <= params[['start']]){
+        value <- "Por favor, o Montante final precisa ser maior que a Entrada."
+        status = "erro"
+  }
+    
+    # Check if Total Money < Monthly Contribution
+  } else if((!is.null(params[['total_money']]) & 
+             !is.null(params[['monthly_contribution']]))){
+    if(params[['total_money']] <= params[['monthly_contribution']]){
+      value <- "Por favor, o Montante final precisa ser maior que a Entrada."
+      status = "erro"
+    }
+  }else{
+    value <- "OK"
+    status <- "OK"
+  }
+  return(list(value = value,
+              status = status))
+}
 # Main ----
 params = list(years = 4,
               var = NULL,
@@ -49,55 +104,11 @@ get_formula(params)
 simulator(params)
 
 simulator <- function(params){
-  
-  qtd_input <- params %>%
-    purrr::map(~length(.)) %>%
-    unlist() %>%
-    sum()
-  
-  # Other conditions: total_money > start, mc ... all > 0
-  
-  # Check if has at least 3 inputs
-  if(qtd_input < 3){
-    msg <- "Por favor, preencha pelo menos três campos."
-    return(list(value = msg, 
-                status = "erro"))
-    
-    # Check if has at least one input missing to simulate
-  } else if(qtd_input == 5){
-    msg <- "Por favor, deixe uma ou duas variáveis em branco. Senão, não há simulação"
-    return(list(value = msg, 
-                status = "erro"))
-    
-    # Check if the relation isn't Anos vs Rentabilidade
-  } else if(is.null(params[['var']]) & is.null(params[['years']])){
-    msg <- "Por favor, não deixe Anos e Rentabilidade em branco ao mesmo tempo."
-    return(list(value = msg, 
-                status = "erro"))
-    
-    # Check if the single simulate isn't Anos
-  } else if(qtd_input == 4 & is.null(params[['years']])){
-    msg <- "Por favor, não deixe apenas o campo Anos em branco."
-    return(list(value = msg, 
-                status = "erro"))
-    
-    # Check if the single simulate isn't Rentabilidade
-  } else if(qtd_input == 4 & is.null(params[['var']])){
-    msg <- "Por favor, não deixe apenas o campo Rentabilidade em branco."
-    return(list(value = msg, 
-                status = "erro"))
-    
+  status_inputs <- check_inputs(params)
+  if(status_inputs[['status']] != "OK"){
+    return("erro")
   }else{
-    start = params[['start']]
-    months = 12*params[['years']]
-    mc = params[['monthly_contribution']]
-    var = params[['var']]
-    final = params[['total_money']]
-    
-    final <- start*(1 + var)^months + mc*(((1 + var)^months) - 1)/var
-    
-    return(list(value = final, 
-                status = "single_simulate"))
+    return("sucesso")
   }
 }
 
